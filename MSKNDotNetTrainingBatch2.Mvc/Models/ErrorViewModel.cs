336b7d818e50f932ca8f01a129444796caab00fe
May Su Kyi Naing <maysukyinaing@gmail.com>
2025-08-23 08:53:48 +0630

    Mvc

---
+++ MSKNDotNetTrainingBatch2.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HAKDotNetTrainingBatch2.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
